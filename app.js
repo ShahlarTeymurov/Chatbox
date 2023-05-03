0415cd10e01f2b548351214223b137ce55a02c86
ShahlarTeymurov <tshahlar@gmail.com>
2023-05-03 11:32:02 +0000

    appjs

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 "use strict";
 $(document).ready(function () {
 
-    // $('h2').click(function (e) { 
-    //     e.preventDefault();
-    //     $('h2').html(1234);
-    // });
+    $('h2').click(function (e) { 
+        e.preventDefault();
+        $('h2').html(1234);
+    });
 
 
 
     $('.btn').click(function (e) { 
         e.preventDefault();
         
-        let a = a => {
+        let a = (a) => {
             for (let i = 1; i <=a; i++) {
                 $('.col-md-12').append(`<h1>${i}</h1>`);
             }
         }
         a($('input').val() )
     });
+
+    
 });
