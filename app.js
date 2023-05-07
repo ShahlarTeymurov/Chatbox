24cf86c32b075ef41f85459627d8d6813d8eb202
ShahlarTeymurov <tshahlar@gmail.com>
2023-05-07 18:45:16 +0000

    js

--- app.js
+++ app.js
@@ -29,9 +29,34 @@ $(document).ready(function () {
 
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-            
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=a48c3222708fb7c8af2bfc6f5e4302bb&units=metric`
+            async function myweather() {
+                let responce = await fetch(url)
+                let data = await responce.json()
+                $("#city").html(data.name)
+                $("#temp").html(data.main.temp)
+                $("#feels").html(data.main.feels_like)
+                $("#myWeather").val("")
+                console.log(data)
+            }
+            myweather()
         }
-    }
+    })
+
+    $("#btn").click(function (e) {
+        e.preventDefault()
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=a48c3222708fb7c8af2bfc6f5e4302bb&units=metric`
+        async function myweather() {
+            let responce = await fetch(url)
+            let data = await responce.json()
+            $("#city").html(data.name)
+            $("#temp").html(data.main.temp)
+            $("#feels").html(data.main.feels_like)
+            $("#myWeather").val("")
+            console.log(data)
+        }
+        myweather()
+
+    })
 
 })
